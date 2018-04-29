6e6f8de8e53c959ee3d8f2ab7b2cc7c8d517d407
Ion.Cojucovschi <ion.cojucovschi@outlook.com>
2018-04-30 00:00:09 +0300

    Add theme for doremi fest

---
+++ newTheme.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/themeStyle.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <img src="imgs/doremiLogo.png" height="40px" width="40px" style="margin-right: 60px; margin-top: 5px;margin-bottom: 5px;">
+      
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-left">
+        <li><a href="">ACASA</a></li>
+        <li><a href="">REGULAMENT</a></li>
+        <li><a href="#portfolio">INREGISTRARE</a></li>
+        <li><a href="#pricing">PARTICIPANTI</a></li>
+        <li><a href="#contact">CONTACTE</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+</head>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+
+
+
+
+
+<!-- Container (About Section) -->
+
+
+
+<footer class="container-fluid text-center">
+ 
+</footer>
+
+
+</body>
+</html>
